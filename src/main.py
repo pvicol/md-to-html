bdcb856a9fd0258540e82d999bc42c4b17f54ef3
pvicol <vicolp1@gmail.com>
2025-04-03 14:42:22 -0700

    Using Github Pages

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
+if len(sys.argv) >= 2:
+    basepath = sys.argv[1]
+else:
+    basepath = '/'
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
@@ -20,7 +25,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
